e62c73ed1239e8d1cf7e8b8d0ccf0e01aebba46e
Darshan kyada <darshankyada02@gmail.com>
2025-03-25 14:47:54 +0530

    Add login registration ejs forms

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 require('dotenv').config();
 const path = require('path');
-const { sequelize } = require('./config/sequelize_db')
-
+const loginRoutes = require('./routes/loginRoute')
 const app = express();
 
 app.use(express.json());
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//Route
+app.use('/login', loginRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
