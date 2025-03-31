8c1d6c3cfc3412f8868eb2e37eb4fc47e2535689
Darshan kyada <darshankyada02@gmail.com>
2025-03-31 18:01:16 +0530

    jQuery validation in registration form

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     const loginForm = $("#LoginForm");
+    const RegisterForm = $("#RegisterForm");
 
     loginForm.validate({
         rules: {
@@ -22,4 +23,44 @@ $(document).ready(function () {
             error.insertAfter(element.closest(".input-group"));
         }
     });
+
+    RegisterForm.validate({
+        rules: {
+            fullName: {
+                required: true
+            },
+            email: {
+                required: true
+            },
+            password: {
+                required: true
+            },
+            confirmPassword: {
+                required: true
+            },
+            terms: {
+                required: true
+            }
+        },
+        messages: {
+            fullName: {
+                required: "Name is required"
+            },
+            email: {
+                required: "Email is required"
+            },
+            password: {
+                required: "Password is required"
+            },
+            confirmPassword: {
+                required: "ConfirmPassword is required"
+            },
+            terms: {
+                required: "Agree is required"
+            }
+        },
+        errorPlacement: function (error, element) {
+            error.insertAfter(element.closest('.input-group'));
+        }
+    })
 });
