ef522c10c850124f540967fb3e3dee3446560b03
Darshan kyada <darshankyada02@gmail.com>
2025-04-03 14:39:21 +0530

    changes in profile page in hobbie field

--- controller/profile-controller.js
+++ controller/profile-controller.js
@@ -4,37 +4,40 @@ const users = require('../models/user');
 // GET
 const profile = async (req, res) => {
     const user = await sessionHelper.loggedUserData(req);
-    res.render("admin/profile", { user });
-}
+    const hobbiesArray = user.hobbies ? user.hobbies.split(",") : [];
+    res.render("admin/profile", {
+        user,
+        hobbiesArray
+    });
+};
 
 // POST
 const profilePage = async (req, res) => {
     try {
 
         const user = req.session.users;
-
         const { f_name, l_name, email, image, number, hobbies, dob, gender } = req.body;
-
         const userid = user.id;
 
-        const updateDetail = await users.update(
-            { f_name, l_name, email, image, number, hobbies, dob, gender },
+        const hobbiesFormate = Array.isArray(hobbies) ? hobbies.join(",") : hobbies;
+
+        await users.update(
+            { f_name, l_name, email, image, number, hobbies: hobbiesFormate, dob, gender },
             { where: { id: userid } },
         );
 
-        // if (updateDetail > 0) {
-            const newUpdate = await users.findOne({
-                where: { id: userid }
-            })
-            req.session.users = newUpdate
-            res.render('admin/profile', {
-                user: newUpdate
-            });
-        // } else {
-        //     res.status(400).json({
-        //         error: "update failed"
-        //     });
-        // }
+        const newUpdate = await users.findOne({
+            where: { id: userid }
+        });
+        
+        req.session.users = newUpdate.get({ plain: true });
+        
+        const hobbiesArray = newUpdate.hobbies ? newUpdate.hobbies.split(",") : [];
+
+        res.render('admin/profile', {
+            user: newUpdate,
+            hobbiesArray
+        });
 
     } catch (error) {
         console.error("user update failed..!", error)
